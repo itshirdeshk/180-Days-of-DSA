51ef19faf68f998601de71f9e68752deddae4edb
Hirdesh Khandelwal <hirdeshkhandelwal58@gmail.com>
2023-09-20 07:43:10 +0530

    Day 3 Code

---
+++ Day 3 - Computer Memory Unit/First.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    std::cout<<"We are Coder Army \n";
+    // So after using namespace std, we don't have to write std again and again
+    cout<<"Hello Everyone \n";
+    cout<<3 + 2 <<endl;
+
+    int a; // Variable declaration
+    a = 10; 
+    cout<<a;
+
+    char c = 'a';
+    cout<<c;
+
+    float f = 2.31;
+    cout<<f;
+
+    double d = 3.121;
+    cout<<d;
+
+    bool b = 0;
+    cout<<b;
+};
