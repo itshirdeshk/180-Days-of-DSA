795fbee9e1d36850c4caf4cd0d66b5c8084313e6
Hirdesh Khandelwal <hirdeshkhandelwal58@gmail.com>
2023-11-10 18:58:15 +0530

    Day 52

--- Day 52 - Strings Interview Questions/Code/code.cpp
+++ Day 52 - Strings Interview Questions/Code/code.cpp
@@ -43,4 +43,43 @@ int longestPalindrome(string s)
     return count + odd;
 }
 
+// Q2. Sorting the Sentence.
+string sortSentence(string s)
+{
+    vector<string> ans(10);
+    string temp;
+    int count = 0, index = 0;
+
+    while (index < s.size())
+    {
+        if (s[index] == ' ')
+        {
+            int pos = temp[temp.size() - 1] - '0';
+            temp.pop_back();
+            ans[pos] = temp;
+            temp.clear();
+            count++;
+        }
+        else
+        {
+            temp += s[index];
+        }
+        index++;
+    }
+    int pos = temp[temp.size() - 1] - '0';
+    temp.pop_back();
+    ans[pos] = temp;
+    temp.clear();
+    count++;
+
+    for (int i = 1; i <= count; i++)
+    {
+        temp += ans[i];
+        temp += ' ';
+    }
+
+    temp.pop_back();
+    return temp;
+}
+
 int main() {}
