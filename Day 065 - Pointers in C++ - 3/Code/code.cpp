795dbf3f25344c127a0cb518f56d2a0075c82ac5
Hirdesh Khandelwal <hirdeshkhandelwal58@gmail.com>
2023-11-28 20:24:29 +0530

    Day 65

--- Day 065 - Pointers in C++ - 3/Code/code.cpp
+++ Day 065 - Pointers in C++ - 3/Code/code.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
 using namespace std;
 
+// Pass by Pointer
+void swapping(int *p1, int *p2)
+{
+    int temp = *p1;
+    *p1 = *p2;
+    *p2 = temp;
+}
+
+// Pass by Reference Variable
+void swapping(int &p1, int &p2)
+{
+    int temp = p1;
+    p1 = p2;
+    p2 = temp;
+}
+
 int main()
 {
     char arr[5] = "1234";
@@ -10,13 +26,31 @@ int main()
 
     cout << (void *)arr << endl;
     cout << static_cast<void *>(arr) << endl;
-    cout << (void *)ptr << endl; 
+    cout << (void *)ptr << endl;
 
     // cout<<&arr<<endl;
 
     char a = 'a';
     char *p = &a;
-    cout<<&a<<endl;
-    cout<<(void *)&a<<endl;
-    cout<<(void *)p<<endl;
+    cout << &a << endl;
+    cout << (void *)&a << endl;
+    cout << (void *)p << endl;
+
+    // Swapping two numbers
+    int first = 10, second = 20;
+    cout << first << " " << second << endl;
+    swapping(&first, &second);
+    cout << first << " " << second << endl;
+
+    // Pass by Reference Variable
+    int num = 10;
+    cout << num << endl;
+    int &temp = num;
+    temp++;
+    cout << num << endl;
+
+    int first = 10, second = 20;
+    cout << first << " " << second << endl;
+    swapping(first, second);
+    cout << first << " " << second << endl;
 }
