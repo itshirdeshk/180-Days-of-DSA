393144d55208a1fa14a22cdc7f17199f3aefba99
Hirdesh Khandelwal <hirdeshkhandelwal58@gmail.com>
2023-11-30 21:55:16 +0530

    Day 66

---
+++ Day 66 - Pointers in C++ - 4/Code/code.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+void fun(int *p){
+    *p = *p + 5;
+}
+
+int main () {
+    int n = 10;
+    int *p = &n;
+    int **p2 = &p;
+    int ***p3 = &p2;
+
+    // cout<<p<<endl;
+    // cout<<p2<<endl;
+    // cout<<p3<<endl;
+
+    // // Value Modification
+    // *p = *p + 5;
+    // **p2 = **p2 + 5;
+    // ***p3 = ***p3 + 5;
+    // cout<<n<<endl;
+
+    fun(p);
+    cout<<n<<endl; 
+}
