80964124c5c27c6ab7fecbd6bbc729a4ff53dc31
Hirdesh Khandelwal <hirdeshkhandelwal58@gmail.com>
2023-12-09 15:20:35 +0530

    Day 75

---
+++ Day 075 - Recursion - 4/Code/code.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+// Q1. Fibonacci Number.
+int fib(int n)
+{
+    if (n <= 1)
+        return n;
+    return fib(n - 1) + fib(n - 2);
+}
+
+// Q2. Climbing Stairs.
+int climbStairs(int n)
+{
+    if (n <= 1)
+        return 1;
+    return climbStairs(n - 1) + climbStairs(n - 2);
+}
+
+// Q3. GCD of two numbers.
+int gcd(int A, int B)
+{
+    // code here
+    if (B == 0)
+        return A;
+    return gcd(B, A % B);
+}
+
+int main() {}
